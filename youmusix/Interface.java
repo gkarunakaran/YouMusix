d77d313b14c0daea9310f8538a25ece76eb932d0
Karanvir Singh <karanvir.singh61@yahoo.in>
2016-09-19 12:35:22 +0530

    Set title as YouMusix

--- youmusix/Interface.java
+++ youmusix/Interface.java
@@ -61,6 +61,7 @@ public class Interface extends JFrame {
 	}
 
 	public Interface() {
+		setTitle("YouMusix");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
